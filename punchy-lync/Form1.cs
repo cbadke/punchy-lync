e83c5cde7c7360676d72160f8c1aacb3d15ddd6d
Curtis Badke <curtis@badkid.ca>
2013-05-19 09:18:45 -0600

    Add winform app with main window hidden

---
+++ punchy-lync/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace punchy_lync
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            //this.Opacity = Double.MinValue;
+            this.Hide();
+        }
+    }
+}
