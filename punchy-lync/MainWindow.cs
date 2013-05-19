5e88057eeb40188eddceb2cdf40d0e1420c4b585
Curtis Badke <curtis@badkid.ca>
2013-05-19 13:10:16 -0600

    Jam all the logics into the main window class. it works!

--- punchy-lync/MainWindow.cs
+++ punchy-lync/MainWindow.cs
@@ -7,16 +7,29 @@ using System.Linq;
 using System.Reflection;
 using System.Resources;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Microsoft.Lync.Model;
+
 namespace punchy_lync
 {
+    public enum Color
+    {
+        Red,
+        Green,
+        Yellow,
+        None
+    }
+
     public partial class MainWindow : Form
     {
         readonly NotifyIcon taskBarIcon;
         ToolStripItem statusItem;
         ToolStripItem messageItem;
+
+        Color _awayColor;
 
         public MainWindow()
         {
@@ -27,6 +40,15 @@ namespace punchy_lync
                 Visible = true,
                 ContextMenuStrip = CreateContextMenu()
             };
+
+            InitializeLight();
+
+            var client = Microsoft.Lync.Model.LyncClient.GetClient();
+            client.Self.Contact.ContactInformationChanged += Status_Changed;
+            var a = (ContactAvailability)client.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+            var n = (String)client.Self.Contact.GetContactInformation(ContactInformationType.PersonalNote);
+
+            UpdateStatus(a, n ?? "");
         }
 
         protected override void OnShown(EventArgs e)
@@ -63,7 +85,143 @@ namespace punchy_lync
         private void Exit_Click(object sender, EventArgs e)
         {
             taskBarIcon.Visible = false;
+            UpdateStatus(ContactAvailability.Offline, "");
             Close();
+        }
+
+        private void Status_Changed(object sender, Microsoft.Lync.Model.ContactInformationChangedEventArgs e)
+        {
+            var client = LyncClient.GetClient();
+            var a = (ContactAvailability)client.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+            var n = (String)client.Self.Contact.GetContactInformation(ContactInformationType.PersonalNote);
+
+            this.Invoke(new MethodInvoker(() =>
+            {
+                UpdateStatus(a, n ?? "");
+            }));
+        }
+
+        private void UpdateStatus(ContactAvailability availability, string message)
+        {
+            statusItem.Text = availability.HumanReadable();
+            messageItem.Text = message;
+
+            switch (availability.ToColor())
+            {
+                case Color.Red:
+                    statusItem.Image = Properties.Resources.red.ToBitmap();
+                    taskBarIcon.Icon = Properties.Resources.red;
+                    break;
+                case Color.Yellow:
+                case Color.None:
+                    statusItem.Image = Properties.Resources.yellow.ToBitmap();
+                    taskBarIcon.Icon = Properties.Resources.yellow;
+                    break;
+                default:
+                    statusItem.Image = Properties.Resources.green.ToBitmap();
+                    taskBarIcon.Icon = Properties.Resources.green;
+                    break;
+            }
+
+            SetLight(availability.ToColor());
+        }
+
+        private void InitializeLight()
+        {
+            var light = Punchy.API.GetLights().FirstOrDefault();
+            if (light == null) return;
+
+            light.SaveColor(System.Drawing.Color.Green, Punchy.Constants.ColorSlot.Color1);
+            light.SaveColor(System.Drawing.Color.Red, Punchy.Constants.ColorSlot.Color2);
+            light.TurnOff();
+
+            _awayColor = Color.Red;
+        }
+
+        private void SetLight(Color c)
+        {
+            var light = Punchy.API.GetLights().FirstOrDefault();
+            if (light == null) return;
+
+            if (c == Color.None)
+            {
+                light.TurnOff();
+            }
+            else if (c == Color.Green)
+            {
+                light.TurnOn(Punchy.Constants.ColorSlot.Color1);
+            }
+            else
+            {
+                if (_awayColor != c)
+                {
+                    if (_awayColor == Color.Red)
+                    {
+                        light.SaveColor(System.Drawing.Color.Yellow, Punchy.Constants.ColorSlot.Color2);
+                        _awayColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        light.SaveColor(System.Drawing.Color.Red, Punchy.Constants.ColorSlot.Color2);
+                        _awayColor = Color.Red;
+                    }
+                }
+
+                light.TurnOn(Punchy.Constants.ColorSlot.Color2);
+            }
+            light.SetBrightness(255);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string HumanReadable(this ContactAvailability c)
+        {
+            switch (c)
+            {
+                case ContactAvailability.Away:
+                    return "Away";
+                case ContactAvailability.Busy:
+                    return "Busy";
+                case ContactAvailability.BusyIdle:
+                    return "Busy";
+                case ContactAvailability.DoNotDisturb:
+                    return "Do Not Disturb";
+                case ContactAvailability.Free:
+                    return "Available";
+                case ContactAvailability.FreeIdle:
+                    return "Idle";
+                case ContactAvailability.None:
+                case ContactAvailability.Invalid:
+                case ContactAvailability.Offline:
+                    return "Offline";
+                case ContactAvailability.TemporarilyAway:
+                    return "Be Right Back";
+                default:
+                    return "";
+            }
+        }
+
+        public static Color ToColor(this ContactAvailability c)
+        {
+            switch (c)
+            {
+                case ContactAvailability.Away:
+                case ContactAvailability.None:
+                case ContactAvailability.TemporarilyAway:
+                    return Color.Yellow;
+                case ContactAvailability.Busy:
+                case ContactAvailability.BusyIdle:
+                case ContactAvailability.DoNotDisturb:
+                    return Color.Red;
+                case ContactAvailability.Free:
+                case ContactAvailability.FreeIdle:
+                    return Color.Green;
+                case ContactAvailability.Offline:
+                case ContactAvailability.Invalid:
+                default:
+                    return Color.None;
+            }
         }
     }
 }
