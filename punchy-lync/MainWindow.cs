37032f4bab070b824ec6ef4e24a2854f43fe01e4
Curtis Badke <curtis@badkid.ca>
2014-02-04 07:56:33 -0700

    moar files!

--- punchy-lync/MainWindow.cs
+++ punchy-lync/MainWindow.cs
@@ -15,47 +15,6 @@ using Microsoft.Lync.Model;
 
 namespace punchy_lync
 {
-    public enum Color
-    {
-        Red,
-        Green,
-        None
-    }
-
-    public class StatusInfo
-    {
-        public Color Color;
-        public String Availability;
-        public String Note;
-
-        public StatusInfo() : this(ContactAvailability.Offline, "", ContactCalendarState.Unknown, "") { }
-
-        public StatusInfo(ContactAvailability Availability, String Note, ContactCalendarState CalendarState, String MeetingSubject)
-        {
-            var availability = Availability;
-            var message = "";
-
-            if (   Availability != ContactAvailability.Busy
-                && Availability != ContactAvailability.DoNotDisturb
-                && Availability != ContactAvailability.Offline)
-            {
-                if (CalendarState == ContactCalendarState.Busy)
-                {
-                    availability = ContactAvailability.Busy;
-                    message = MeetingSubject ?? Note ?? "";
-                }
-                else
-                {
-                    availability = ContactAvailability.Free;
-                }
-            }
-
-            this.Availability = availability.HumanReadable();
-            this.Color = availability.ToColor();
-            this.Note = message;
-        }
-    }
-
     public partial class MainWindow : Form
     {
         readonly NotifyIcon taskBarIcon;
@@ -201,57 +160,6 @@ namespace punchy_lync
                 light.TurnOn(Punchy.Constants.ColorSlot.Color2);
             }
             light.SetBrightness(255);
-        }
-    }
-
-    public static class Extensions
-    {
-        public static string HumanReadable(this ContactAvailability c)
-        {
-            switch (c)
-            {
-                case ContactAvailability.Away:
-                    return "Away";
-                case ContactAvailability.Busy:
-                    return "Busy";
-                case ContactAvailability.BusyIdle:
-                    return "Busy";
-                case ContactAvailability.DoNotDisturb:
-                    return "Do Not Disturb";
-                case ContactAvailability.Free:
-                    return "Available";
-                case ContactAvailability.FreeIdle:
-                    return "Idle";
-                case ContactAvailability.None:
-                case ContactAvailability.Invalid:
-                case ContactAvailability.Offline:
-                    return "Offline";
-                case ContactAvailability.TemporarilyAway:
-                    return "Be Right Back";
-                default:
-                    return "";
-            }
-        }
-
-        public static Color ToColor(this ContactAvailability c)
-        {
-            switch (c)
-            {
-                case ContactAvailability.Away:
-                case ContactAvailability.TemporarilyAway:
-                case ContactAvailability.Free:
-                case ContactAvailability.FreeIdle:
-                    return Color.Green;
-                case ContactAvailability.Busy:
-                case ContactAvailability.BusyIdle:
-                case ContactAvailability.DoNotDisturb:
-                    return Color.Red;
-                case ContactAvailability.Offline:
-                case ContactAvailability.Invalid:
-                case ContactAvailability.None:
-                default:
-                    return Color.None;
-            }
         }
     }
 }
